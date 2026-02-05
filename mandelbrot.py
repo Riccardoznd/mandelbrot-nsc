a83e66bff06613244b6a652c0850b7b06db5430d
Riccardoznd <ZX76SL@student.aau.dk>
2026-02-05 15:03:05 +0100

    region definition added plus evenly spaced vectors

--- mandelbrot.py
+++ mandelbrot.py
@@ -4,20 +4,21 @@ Mandelbrot Set Generator
 Author: Riccardo Zanda
 Course: Numerical Scientific Computing 2026
 """
+import numpy as np
 
-def f(x):
-    """
-    Example function.
+#define region boundaries
+x_min=-2.0
+x_max=1.0
+y_min=-1.5
+y_max=1.5
 
-    Parameters
-    ----------
-    x : float
-        Input value
+#define resolution
+width=1024
+height=1024
 
-    Returns
-    -------
-    float
-        Output value
-    """
+#evenly spaced vectors
+x=np.linspace(x_min,x_max,width)
+y=np.linspace(y_min,y_max,height)
+
     # TODO: Implement the algorithm
     pass
