1f9460cd7b79d4208d576f55104fe930ed6ba481
Riccardoznd <ZX76SL@student.aau.dk>
2026-02-27 14:37:12 +0100

    mandelbrot with numpy first correct draft

--- mandelbrot.py
+++ mandelbrot.py
@@ -5,6 +5,9 @@ Author: Riccardo Zanda
 Course: Numerical Scientific Computing 2026
 """
 import numpy as np
+import time
+import statistics
+import matplotlib.pyplot as plt
 
 #define region boundaries
 x_min=-2.0
@@ -28,3 +31,24 @@ C= X +1j *Y
 
 print(f"shape: {C.shape}") #1024,1024
 print(f"Type:{C.dtype}") #complex128
+
+#now i inizialize Z used for the current value and M for the the iteration counting map
+Z = np.zeros_like(C) #_like makes it so that Z is also a complex number array
+M = np.zeros(C.shape, dtype=int)
+
+#we still need to make a loop because the formula of the mandelbrot set is recursive
+max_iter=100
+
+for n in range(max_iter):
+        z = np.abs(Z) <= 2 #z acts as a mask
+        Z[z] = Z[z]**2 + C[z]
+        
+        # Increment iteration count for those points
+        M[z] += 1
+        
+    return M
+
+plt.figure(figsize=(10, 10))
+plt.imshow(M_result, extent=[-2, 1, -1.5, 1.5], origin='lower', cmap='hot')
+plt.colorbar()
+plt.show()
