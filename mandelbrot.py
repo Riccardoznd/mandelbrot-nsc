eea4d436d5e66e517a62935512caa68ecada17df
Riccardoznd <ZX76SL@student.aau.dk>
2026-02-28 18:18:22 +0100

    changed it to a fucntion in order to be able to implement benchmark

--- mandelbrot.py
+++ mandelbrot.py
@@ -18,34 +18,40 @@ y_max=1.5
 #define resolution
 width=1024
 height=1024
-
-#evenly spaced vectors
-x=np.linspace(x_min,x_max,width)
-y=np.linspace(y_min,y_max,height)
-
-#creating 2D map of the coordinates
-X,Y=np.meshgrid(x,y)
-
-#make complex numbers
-C= X +1j *Y
-
-print(f"shape: {C.shape}") #1024,1024
-print(f"Type:{C.dtype}") #complex128
-
-#now i inizialize Z used for the current value and M for the the iteration counting map
-Z = np.zeros_like(C) #_like makes it so that Z is also a complex number array
-M = np.zeros(C.shape, dtype=int)
-
-#we still need to make a loop because the formula of the mandelbrot set is recursive
 max_iter=100
 
-for n in range(max_iter):
-    z = np.abs(Z) <= 2 #z acts as a mask
-    Z[z] = Z[z]**2 + C[z]
+def my_mandelbrot(x_min, x_max, y_min, y_max, width, height, max_iter):
+
+    #evenly spaced vectors
+    x=np.linspace(x_min,x_max,width)
+    y=np.linspace(y_min,y_max,height)
+
+    #creating 2D map of the coordinates
+    X,Y=np.meshgrid(x,y)
+
+    #make complex numbers
+    C= X +1j *Y
+
+    print(f"shape: {C.shape}") #1024,1024
+    print(f"Type:{C.dtype}") #complex128
+
+    #now i inizialize Z used for the current value and M for the the iteration counting map
+    Z = np.zeros_like(C) #_like makes it so that Z is also a complex number array
+    M = np.zeros(C.shape, dtype=int)
+
+    #we still need to make a loop because the formula of the mandelbrot set is recursive
+    max_iter=100
+
+    for n in range(max_iter):
+        z = np.abs(Z) <= 2 #z acts as a mask
+        Z[z] = Z[z]**2 + C[z]
         
-# Increment iteration count for those points
-    M[z] += 1
-        
+        # Increment iteration count for those points
+        M[z] += 1
+    return M 
+
+#using now the fucntion
+M = my_mandelbrot(x_min, x_max, y_min, y_max, width, height, max_iter)
 
 
 plt.figure(figsize=(10, 10))
