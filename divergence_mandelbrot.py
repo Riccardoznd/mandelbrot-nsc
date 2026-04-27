7b8d586442973103e2fa9cd031afc5a5a2951a99
Riccardoznd <ZX76SL@student.aau.dk>
2026-04-27 14:54:57 +0200

    add imports and equally spaced vectors

---
+++ divergence_mandelbrot.py
@@ -0,0 +1,8 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.colors import LogNorm
+
+# Resolution and equally spaced vectors
+N, MAX_ITER = 512, 1000
+x = np.linspace(-0.7530, -0.7490, N)
+y = np.linspace(0.0990, 0.1030, N)
